3d4fb73a4d881a5b08df7d3e9c2c022d15d17e50
james-boge <James_boge@163.com>
2019-06-11 10:11:24 +0800

    the first test is successful

---
+++ src/main/java/Test1.java
@@ -0,0 +1,21 @@
+import java.util.HashMap;
+
+public class Test1 {
+
+    public static void main(String[] args) {
+
+        String name = "james_boge";
+        System.out.println(name);
+        HashMap hashMap = new HashMap();
+    }
+}
+
+
+
+
+
+
+
+
+
+
